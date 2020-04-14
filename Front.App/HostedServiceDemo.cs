9bdcdf31c0b9185b129b2d5b9cf7bb3959e02c3e
Olivier Spinelli <olivier.spinelli@signature-code.com>
2020-04-14 17:31:21 +0200

    Uses new Type/Kind definition.

---
+++ Front.App/HostedServiceDemo.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CK.Core;
+
+namespace Front.App
+{
+    /// <summary>
+    /// This is a demo hosted service: this is automatically registered as a hosted service and
+    /// start/stop methods are called by the host.
+    /// Since this is a Singleton service, other participants can perfectly depends on this class (or
+    /// a ISingletonAutoService interface can be defined)
+    /// </summary>
+    public class HostedServiceDemo : IHostedService
+                                     //, ISingletonAutoService
+                                     // --> This is useless: Microsoft.Extensions.Hosting.IHostedService is automatically
+                                     //     registered as a [IsMultiple] Singleton service.
+    {
+        public Task StartAsync( CancellationToken cancellationToken )
+        {
+            HasBeenStarted = true;
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync( CancellationToken cancellationToken )
+        {
+            HasBeenStopped = true;
+            return Task.CompletedTask;
+        }
+
+        public bool HasBeenStarted { get; private set; }
+
+        public bool HasBeenStopped { get; private set; }
+
+        public bool IsRunning => HasBeenStarted && !HasBeenStopped;
+
+    }
+}
