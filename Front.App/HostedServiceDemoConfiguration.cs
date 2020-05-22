a2921515f23dee59f224c00b76e3e7fec531a618
Olivier Spinelli <olivier.spinelli@signature-code.com>
2020-05-22 17:20:27 +0200

    Added IOptionsMonitor<> demo & fix CKSetup.xml type definitions.

---
+++ Front.App/HostedServiceDemoConfiguration.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Front.App
+{
+    public class HostedServiceDemoConfiguration
+    {
+        public string MessageToTheWorld { get; set; }
+    }
+}
